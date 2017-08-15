d43d1921e35549579c6022c62f0aef4600b72f09
yuantang <1055410108@qq.com>
2017-08-15 16:13:35 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const webpack = require('webpack')
+const path  = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const port = 9000;
+
+module.exports = {
+    entry: {
+        app: [
+            'babel-polyfill', 
+            './src/index.js'
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                use: ['babel-loader']
+            }, {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }, {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },{
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: '/images/[name].[ext]'
+                }
+            }, {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: '/fonts/[name].[ext]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        },
+        extensions: ['.js', '.jsx', '.styl']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        open: true,
+        openPage: '',
+        port: port,
+        proxy: {
+            // '/api': 'http://localhost:3000'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.tpl.html',
+            inject: 'body'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"development"',
+        }),
+    ]
+}
---
+++ webpack.production.js
@@ -0,0 +1,108 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const port = 9000;
+
+module.exports = {
+    entry: {
+        app: [
+            'babel-polyfill',
+            './src/index.js'
+        ]
+    },
+    output: {
+        filename: '[name].[hash:8].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.(jsx|js)$/,
+            exclude: [
+                path.resolve(__dirname, 'node_modules')
+            ],
+            use: ['babel-loader']
+        }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader','postcss-loader']
+            })
+        }, {
+            test: /\.styl$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'stylus-loader',
+                        options: {
+                            sourceMap: false
+                        }
+                    }
+                ]
+            })
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            loader: 'url-loader',
+            query: {
+                limit: 10000,
+                name: '/images/[name].[hash:8].[ext]'
+            }
+        }, {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            query: {
+                limit: 10000,
+                name: '/fonts/[name].[hash:8].[ext]'
+            }
+        }]
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        },
+        extensions: ['.js', '.jsx', '.styl']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.tpl.html',
+            inject: 'body'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"',
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+            },
+            sourceMap: true
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/[name].[hash:8].css'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module, count) {
+                return (
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(
+                        path.join(__dirname, '../node_modules')
+                    ) === 0
+                )
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            chunks: ['vendor']
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+}
